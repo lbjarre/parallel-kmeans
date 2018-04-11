8a086f9427f5800b08181c8974a87b76e4ef4809
Lukas Bjarre <lbjarre@kth.se>
2018-04-11 15:30:21 +0200

    Fixed incorrect initialization of distance sum

--- src/main.c
+++ src/main.c
@@ -2,54 +2,53 @@
 #include <stdio.h>
 #include <math.h>
 
+
 double dist_sq(double *x, double *y, int len)
 {
-    double sum;
+    double sum = 0;
     for (int i = 0; i < len; ++i) {
         sum += (x[i] - y[i]) * (x[i] - y[i]);
     }
     return sum;
 }
 
-int nearest_mean(double *x, double **means, int len_x, int len_m)
+
+int nearest_mean(double *x, double **means, int dim_x, int k)
 {
     double dist;
     double min = INFINITY;
     int i_min = -1;
     
-    for (int i = 0; i < len_m; ++i) {
-        dist = dist_sq(x, means[i], len_x);
+    for (int i = 0; i < k; ++i) {
+        dist = dist_sq(x, means[i], dim_x);
         if (dist < min) {
             min = dist;
             i_min = i;
         }
     }
-
     return i_min;
 }
+
 
 double ** k_means(double **x, int dim_x, int len_x, int k)
 {
     double **m, **m_next;
     int k_count[k];
-    int x_class[len_x];
     int curr_class;
     int not_converged = 1;
+    int iter = 0;
 
     m = malloc(k * sizeof m);
     m_next = malloc(k * sizeof m_next);
     /*  Randomly initialize the initial class means
     */
     for (int i = 0; i < k; ++i) {
-        m[i] = malloc(dim_x * sizeof m[i]);
-        m_next[i] = calloc(dim_x, sizeof m_next[i]);
+        m[i] = malloc(dim_x * sizeof *m);
+        m_next[i] = calloc(dim_x, sizeof *m_next);
         for (int j = 0; j < dim_x; ++j) {
             m[i][j] = (double) rand() / RAND_MAX;
         }
     }
-
-    /*  Main loop: number of total iterations
-    */
 
     do {
         /*  Classify all of the data points to the current class means
@@ -58,7 +57,6 @@ double ** k_means(double **x, int dim_x, int len_x, int k)
         */
         for (int j = 0; j < len_x; ++j) {
             curr_class = nearest_mean(x[j], m, dim_x, k);
-            x_class[j] = curr_class;
             k_count[curr_class]++;
             for (int l = 0; l < dim_x; ++l) {
                 m_next[curr_class][l] += x[j][l];
@@ -69,7 +67,7 @@ double ** k_means(double **x, int dim_x, int len_x, int k)
         */
         for (int j = 0; j < k; ++j) {
             for (int l = 0; l < dim_x; ++l) {
-                m_next[j][l] /= (k_count[j] == 0) ? 1 : k_count[j]; // Ternary operator to guard against 0 division
+                m_next[j][l] /= (k_count[j] == 0) ? 1 : k_count[j]; // Guarding against 0 division
                 k_count[j] = 0; // Zero out the bins for next iteration
             }
         }
@@ -86,17 +84,17 @@ double ** k_means(double **x, int dim_x, int len_x, int k)
                 m_next[j][l] = 0;
             }
         }
-
-    } while (not_converged);
+    } while (not_converged && iter++ < 100);
 
     return m;
 }
 
+
 int main(int argc, char **agrv)
 {
-    int dim_x = 5;
-    int len_x = 10;
-    int k = 3;
+    int dim_x = 2;
+    int len_x = 50;
+    int k = 4;
     double **x, **m;
 
     x = malloc(len_x * sizeof x);
