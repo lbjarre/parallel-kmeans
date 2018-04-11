966742fe540db65f83e35af61eeb76fa92014422
Lukas Bjarre <lbjarre@kth.se>
2018-04-11 11:24:26 +0200

    Implemented serial version of k-means

--- src/main.c
+++ src/main.c
@@ -19,7 +19,6 @@ int nearest_mean(double *x, double **means, int len_x, int len_m)
     
     for (int i = 0; i < len_m; ++i) {
         dist = dist_sq(x, means[i], len_x);
-        printf("dist: %f\n", dist);
         if (dist < min) {
             min = dist;
             i_min = i;
@@ -29,41 +28,94 @@ int nearest_mean(double *x, double **means, int len_x, int len_m)
     return i_min;
 }
 
-int main(int argc, char **agrv)
+double ** k_means(double **x, int dim_x, int len_x, int k)
 {
-    int len = 5;
-    int len_m = 3;
-    double *x, *y, **m;
+    double **m;
+    int k_count[k];
+    int x_class[len_x];
+    int curr_class;
 
-    x = malloc(len * sizeof x);
-    y = malloc(len * sizeof y);
+    m = malloc(k * sizeof m);
     
-    srand(14);
-
-    for (int i = 0; i < len; ++i) {
-        x[i] = (double) rand() / RAND_MAX;
-        y[i] = (double) rand() / RAND_MAX;
-    }
-
-    m = malloc(len_m * sizeof m);
-    
-    for (int i = 0; i < len_m; ++i) {
-        m[i] = malloc(len * sizeof m);
-        for (int j = 0; j < len; ++j) {
+    /*  Randomly initialize the initial class means
+    */
+    for (int i = 0; i < k; ++i) {
+        m[i] = malloc(dim_x * sizeof m[i]);
+        for (int j = 0; j < dim_x; ++j) {
             m[i][j] = (double) rand() / RAND_MAX;
         }
     }
 
-    double dist = dist_sq(x, y, len);
-    printf("%f\n", dist);
+    /*  Main loop: number of total iterations
+    */
+    for (int i = 0; i < 10; ++i) {
 
-    int i_x = nearest_mean(x, m, len, len_m);
-    int i_y = nearest_mean(y, m, len, len_m);
-    printf("x: %d, y: %d\n", i_x, i_y);
+        /*  Classify all of the data points to the current class means
+            x_class keeps track of which class each data point belongs to
+            k_count keeps track of the number of data points for each class
+        */
+        for (int j = 0; j < len_x; ++j) {
+            curr_class = nearest_mean(x[j], m, dim_x, k);
+            x_class[j] = curr_class;
+            k_count[curr_class]++;
+        }
 
-    free(x);
-    free(y);
-    free(m);
+        /*  Zero out the class means before calculating the new values
+        */
+        for (int j = 0; j < k; ++j) {
+            for (int l = 0; l < dim_x; ++l) {
+                m[j][l] = 0;
+            }
+        }
+
+        /*  Add up the vector values of all classified points
+        */
+        for (int j = 0; j < len_x; ++j) {
+            curr_class = x_class[j];
+            for (int l = 0; l < dim_x; ++l) {
+                m[curr_class][l] += x[j][l];
+            }
+        }
+
+        /*  Divide by number of classified points to get the new mean
+        */
+        for (int j = 0; j < k; ++j) {
+            for (int l = 0; l < dim_x; ++l) {
+                m[j][l] /= (k_count[j] == 0) ? 1 : k_count[j]; // Ternary operator to guard against 0 division
+                k_count[j] = 0;
+            }
+        }
+    }
+
+    return m;
+}
+
+int main(int argc, char **agrv)
+{
+    int dim_x = 5;
+    int len_x = 10;
+    int k = 3;
+    double **x, **m;
+
+    x = malloc(len_x * sizeof x);
+    
+    srand(14);
+
+    for (int i = 0; i < len_x; ++i) {
+        x[i] = malloc(dim_x * sizeof x[i]);
+        for (int j = 0; j < dim_x; ++j) {
+            x[i][j] = (double) rand() / RAND_MAX;
+        }
+    }
+
+    m = k_means(x, dim_x, len_x, k);
+
+    for (int i = 0; i < k; i++) {
+        printf("Class %d\n", i);
+        for (int j = 0; j < dim_x; ++j) {
+            printf("%f\n", m[i][j]);
+        }
+    }
 
     return 0;
 }
