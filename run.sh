b4650c779111ad2ec110fad3ee40e2ceb96d7934
Lukas Bjarre <lbjarre@kth.se>
2018-04-23 12:25:08 +0200

    Removed compilation step from run script

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 # Kmeans: path - xdim - xlen - K
 # time bin/main data/Phones_accelerometer.csv 6 13062476 2
-mpicc src/parallel.c -o bin/parallel
 # time mpirun bin/parallel data/Phones_accelerometer.csv 6 13062476 2 -np 2
 time mpirun bin/parallel data/iris.csv 4 150 2 -np 2
