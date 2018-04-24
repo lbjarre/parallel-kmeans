0b40630f776717881d8ef82bbce25821cd50211a
Gabriel Carrizo <gabriel.carrizo@now.se>
2018-04-24 17:56:17 +0200

    new approach to parallel read file.

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 # Kmeans: path - xdim - xlen - K
-# time bin/main data/Phones_accelerometer.csv 6 13062475 2
-# time mpirun bin/parallel data/Phones_accelerometer.csv 6 13062475 2 -np 2
+# time mpirun bin/read data/iris_trim.csv 3 150 2 -np 2
 time mpirun bin/read data/data_trim.csv 5 13062475 2 -np 2
--- src/readparallel.c
+++ src/readparallel.c
@@ -91,107 +91,79 @@ double ** assignSeeds(double **data, int *seeds, int K, int dim_x)
 }
 
 
-double ** read_parallel_csv(MPI_File in, const int p, const int P, const int overlap, int len_x, int dim_x)
+double ** char_to_double(char *local_file_partition, const int dim_x, const int I, const int x_cols)
+{
+  char *token, *line;
+  double *x_line, **x;
+  int c = 0, n = 0, i;
+
+  x = malloc(I * sizeof x);
+  for (i = 0; i < I; i++) {
+    line = malloc(x_cols * sizeof line);
+    x_line = malloc(dim_x * sizeof x_line);
+    for (int j = 0; j < x_cols; j++) {
+      line[j] = local_file_partition[i*x_cols + j];
+    }
+    token = strtok(line, ",");
+    while (token) {
+        x_line[c++] = strtod(token, NULL);
+        token = strtok(NULL, ",");
+    }
+    c = 0;
+    x[i++] = x_line;
+    free(line);
+    free(x_line);
+  }
+
+  return x;
+}
+
+
+double ** read_parallel_csv(MPI_File in, const int p, const int P, const int overlap, int len_x, int dim_x, const int I)
 {
   /*
     Reads from file in parallel.
   */
-  int sum, local_file_size, local_start, local_end, I, i, j, c, n;
-  char *local_file_partition, *token, *line;
-  double *x_line;
-  double ** x;
-  MPI_Offset file_size, global_end, global_start;
+  int x_cols, i, local_file_size;
+  char *local_file_partition, *buffer_file;
+  double **x;
+  MPI_Offset file_size, global_start;
 
   //get total file size
   MPI_File_get_size(in, &file_size);
-  file_size--;
-  printf("Total file size: %d\n", file_size);
 
-  //calculate local file size
-  local_file_size = (file_size + P - p - 1) / P +1 ;
+  //reads the first lines in file to calculate how and what to read
+  buffer_file = malloc(overlap * sizeof buffer_file);
+  MPI_File_read_at_all(in, 0, buffer_file, overlap, MPI_CHAR, MPI_STATUS_IGNORE);
 
-  //calculate absolute file start and top index
+  x_cols = 0;
+  while(buffer_file[x_cols] != '\n'){
+    x_cols++;
+  }
+  x_cols++; //to account for new line
+
+  local_file_size = I * x_cols;
+
   global_start = 0;
-  for (int i = 0; i < p; i++) {
-    global_start = global_start + (file_size + P - i -1) / P;
-  }
-  global_end = global_start + local_file_size - 1;
-
-  if (p != P-1) {
-    local_file_size += overlap;
+  for (i = 0; i < p; i++) {
+    global_start += (len_x + P - i - 1) / P;
   }
 
-  //allocate space for local part of file, specific for each process with
-  local_file_partition = malloc(local_file_size * sizeof local_file_partition + 1);
+  local_file_partition = malloc(local_file_size * sizeof local_file_partition);
 
-  /*Read file in, from global_start, a total of local_file_size bytes.
-   Save to local_file_partition */
-  MPI_File_read_at_all(in, global_start, local_file_partition, local_file_size, MPI_CHAR, MPI_STATUS_IGNORE);
-  local_file_partition[local_file_size] = '\n'; //set last index to NL. Will use this later.
+  MPI_File_read_at_all(in, global_start*x_cols, local_file_partition, local_file_size, MPI_CHAR, MPI_STATUS_IGNORE);
 
+  x = char_to_double(local_file_partition, dim_x, I, x_cols);
 
-  //Trim both file ends as the file most likely did not stop dividing on new line.
-  local_start = 0;
-  local_end = local_file_size ;
-  printf("%d\n", local_start);
-  printf("%d\n", local_end);
-
-  /*
-
-  if (p != 0) {
-      while(local_file_partition[local_start] != '\n'){
-        printf("%c", local_file_partition[local_start]);
-        local_start++;
-      }
-      printf("\n");
-      local_start++;
-  }
-  if (p != P-1){
-      local_end-=overlap;
-      while(local_file_partition[local_end] != '\n') {
-        local_end++;
-        printf("%c", local_file_partition[local_end]);
-      }
-      local_end++;
-      printf("\n");
-  }
-  local_file_size = local_end - local_start + 1;
-  */
-
-  //convert str to char
-  I = (len_x + P - p - 1) / P;
-  x = malloc(I * sizeof x);
-  line = malloc(overlap*2 * sizeof line);
-  j = 0; i = 0; c = 0;
-  /*if(p==1){
-    printf("Local start: %d \nLocal end: %d\nLocal size: %d\n ", local_start, local_end, local_file_size);
-  }*/
-  for (n = local_start; n < local_end; n++) {
-    if (local_file_partition[n] != '\n') {
-      line[j++] = local_file_partition[n];
-    }
-    else{
-      x_line = malloc(dim_x * sizeof x_line);
-      token = strtok(line, ",");
-      while(token) {
-        x_line[c++] = strtod(token, NULL);
-        token = strtok(NULL, ",");
-      }
-      c = 0; j = 0;
-      x[i++] = x_line;
-      free(line);
-      line = malloc(overlap * 2 * sizeof line);
-    }
-  }
   return x;
 }
 
 
 int main(int argc, char **argv)
 {
-  int rank, tag, rc, displs, P, p, I;
-  int *sendcounts, *offset, *seeds;
-  double **data, **x, **m, **rec_buf;
+  int tag, rc, P, p, I;
+  int *offset;
+  double **x;
 
   int dim_x = 4;
   int len_x = 92;
@@ -235,12 +207,13 @@ int main(int argc, char **argv)
 
   MPI_File_open(MPI_COMM_WORLD, file_name, MPI_MODE_RDONLY, MPI_INFO_NULL, &fp);
 
-  x = read_parallel_csv(fp, p, P, overlap, len_x, dim_x);
-
   I = (len_x + P - p - 1) / P;
 
+  x = read_parallel_csv(fp, p, P, overlap, len_x, dim_x, I);
+
+
   if (p == 1) {
-    //printPartition(x, I, dim_x);
+    printPartition(x, I, dim_x);
   }
 
   /*
