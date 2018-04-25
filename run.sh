220607f6e1c1b6cf208f9285691ef4f8e64a3267
Lukas Bjarre <lbjarre@kth.se>
2018-04-25 13:52:23 +0200

    Added argument passing to run script

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if ["$1" != ""]; then
+  binfile="parallel"
+else
+  binfile="$1"
+fi
+
 # Kmeans: path - xdim - xlen - K
- time mpirun bin/read data/iris_trim.csv 3 149 2 -np 2
-# time mpirun bin/read data/data_trim.csv 5 13062475 2 -np 2
+time mpirun bin/$binfile data/iris_trim.csv 3 149 2 -np 2
